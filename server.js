2089f616004dc9c1b92a801c0db76face03652d6
Loopfelloff <abhiyanregmi@gmail.com>
2025-09-26 11:28:49 +0545

    added the initial files for our discord bot

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+const {Client , Events , GatewayIntentBits} = require('discord.js')
+const token = process.env.DiSCORD_BOT_TOKEN
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds]})
+
+client.once(Events.ClientReady, readyClient =>{
+    console.log("Ready! Logged in as ${readyClient.user.tag}")
+})
+
+client.login(token)
